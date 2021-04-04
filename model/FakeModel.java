7b6d24a2a753d3c0b82fb8b48e725b8e9589f151
Michael Nakhimson <michael.nakhimson@gmail.com>
2021-04-04 14:04:53 +0300

    Initial commit

---
+++ model/FakeModel.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(new User("Vasya"+i,i, i));
+        }
+        modelData.setUsers(users);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void loadUserById(long userId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void changeUserData(String name, long id, int level) {
+        throw new UnsupportedOperationException();
+    }
+}
